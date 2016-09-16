471518928f8e97fc4386d22c9898a9add493d1ee
Giacomo Vacca <giacomo.vacca@gmail.com>
2016-09-16 15:16:14 +0200

    Support tests for basic authentication

--- kamailio_testing/sipp/scenarios/run_sipp.sh
+++ kamailio_testing/sipp/scenarios/run_sipp.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-sipp -sf hac_HTTP_GET.xml kamailio_async -r 1 -m 1
-sipp -sf hac_HTTPS_GET.xml kamailio_async -r 1 -m 1
-sipp -sf hac_HTTP_POST_body.xml kamailio_async -r 1 -m 1
-sipp -sf hac_HTTPS_POST_body.xml kamailio_async -r 1 -m 1
+#sipp -sf hac_HTTP_GET.xml kamailio_async -r 1 -m 1
+#sipp -sf hac_HTTPS_GET.xml kamailio_async -r 1 -m 1
+
+#sipp -sf hac_HTTP_POST_body.xml kamailio_async -r 1 -m 1
+#sipp -sf hac_HTTPS_POST_body.xml kamailio_async -r 1 -m 1
+
+# Basic authentication - must pass username and password
+#sipp -sf hac_HTTP_GET_basicauth_200.xml kamailio_async -r 1 -m 1
+
+# Basic authentication - pass wrond username/password
+sipp -sf hac_HTTP_GET_basicauth_401.xml kamailio_async -r 1 -m 1
