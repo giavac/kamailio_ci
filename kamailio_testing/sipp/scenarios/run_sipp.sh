aaefc4cbc4fc2a03e5d995df79d0c987a048f335
Giacomo Vacca <giacomo.vacca@gmail.com>
2016-09-16 17:26:09 +0200

    Stashing nginx configuration with basic auth and used scenarios

--- kamailio_testing/sipp/scenarios/run_sipp.sh
+++ kamailio_testing/sipp/scenarios/run_sipp.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 
-#sipp -sf hac_HTTP_GET.xml kamailio_async -r 1 -m 1
-#sipp -sf hac_HTTPS_GET.xml kamailio_async -r 1 -m 1
+# Simplest GET
+sipp -sf hac_HTTP_GET.xml kamailio_async -r 1 -m 1
 
-#sipp -sf hac_HTTP_POST_body.xml kamailio_async -r 1 -m 1
-#sipp -sf hac_HTTPS_POST_body.xml kamailio_async -r 1 -m 1
+# HTTPS GET
+sipp -sf hac_HTTPS_GET.xml kamailio_async -r 1 -m 1
+
+# HTTP POST
+sipp -sf hac_HTTP_POST_body.xml kamailio_async -r 1 -m 1
+
+# HTTPS POST
+sipp -sf hac_HTTPS_POST_body.xml kamailio_async -r 1 -m 1
 
 # Basic authentication - must pass username and password
 sipp -sf hac_HTTP_GET_basicauth_200.xml kamailio_async -r 1 -m 1
 
 # Basic authentication - pass wrong username/password
-#sipp -sf hac_HTTP_GET_basicauth_401.xml kamailio_async -r 1 -m 1
+sipp -sf hac_HTTP_GET_basicauth_401.xml kamailio_async -r 1 -m 1
 
 # Basic authentication - pass no credentials
-#sipp -sf hac_HTTP_GET_basicauth_no_username_401.xml kamailio_async -r 1 -m 1
+sipp -sf hac_HTTP_GET_basicauth_no_username_401.xml kamailio_async -r 1 -m 1
