5b85b95e3b209de821a07756adc62c141534b489
Giacomo Vacca <giacomo.vacca@gmail.com>
2016-09-16 10:56:48 +0200

    Start generalising kamailio.cfg and instructing what query and what params to use via SIP

--- kamailio_testing/sipp/scenarios/run_sipp.sh
+++ kamailio_testing/sipp/scenarios/run_sipp.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-sipp -sf msg.xml kamailio_async -r 1 -m 1
+sipp -sf hac_HTTP_GET.xml kamailio_async -r 1 -m 1
+sipp -sf hac_HTTPS_GET.xml kamailio_async -r 1 -m 1
+sipp -sf hac_HTTP_POST_body.xml kamailio_async -r 1 -m 1
+sipp -sf hac_HTTPS_POST_body.xml kamailio_async -r 1 -m 1
